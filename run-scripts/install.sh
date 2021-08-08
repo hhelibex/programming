ca07c81d573fea3d906c8a0b0f9d4c94527ba912
HHeLiBeX <hhelibex@riddling-world.net>
2021-08-08 12:20:47 +0900

    GNU timeが必要である旨のメッセージを出力。

--- run-scripts/install.sh
+++ run-scripts/install.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+if [ ! -x /usr/bin/time ]; then
+	printf "GNU time package must be installed.\n"
+	printf "    ex. yum -y install time\n"
+	exit 1
+fi
+
 if [ -z ${1} ]; then
 	printf "Usage: %s <dir>\n" ${0}
 	exit 1
